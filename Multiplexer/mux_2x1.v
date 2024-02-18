a76a5a2d3287dd72ac366db610bb581ffc9d9c07
ShravanKumar-21 <57145455+ShravanKumar-21@users.noreply.github.com>
2024-02-18 13:59:17 +0530

    Verilog design code for 2x1 multiplxer

--- Multiplexer/mux_2x1.v
+++ Multiplexer/mux_2x1.v
@@ -1,1 +1,21 @@
+`timescale 1ns / 1ps
 
+module mux_12(
+    input a,
+    input b, 
+    output reg y,
+    input sel
+    );
+    
+    always@(*)
+    begin
+       if( sel == 1'b0)
+       begin
+          y = a;
+       end
+       else
+       begin
+          y = b;
+       end
+    end
+endmodule
